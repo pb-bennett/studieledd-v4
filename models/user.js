2fc7f77a8474f71c89e743c16ce5513f4f931778
pb-bennett <97233485+pb-bennett@users.noreply.github.com>
2023-11-15 13:11:57 +0000

    removed unique property from "name" in user schema

--- models/user.js
+++ models/user.js
@@ -5,7 +5,6 @@ const userSchema = new mongoose.Schema(
     name: {
       type: String,
       required: [true, "Please provide a name"],
-      unique: true,
     },
     email: {
       type: String,
